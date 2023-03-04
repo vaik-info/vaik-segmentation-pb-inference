3dd5b60f5790d5801790a4b3cb63db22adbf5b2b
vaik-info <info@vaikobo.com>
2023-03-05 07:44:29 +0900

    first commit

--- vaik_segmentation_pb_inference/pb_model.py
+++ vaik_segmentation_pb_inference/pb_model.py
@@ -2,7 +2,7 @@ from typing import List, Dict, Tuple
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-
+from skimage.transform import resize
 
 class PbModel:
     def __init__(self, input_saved_model_dir_path: str = None, classes: Tuple = None):
@@ -13,10 +13,10 @@ class PbModel:
         self.model_output_dtype = self.model.signatures["serving_default"].outputs[0].dtype
         self.classes = classes
 
-    def inference(self, input_image_list: List[np.ndarray], batch_size: int = 8) -> Tuple[List[Dict], Dict]:
-        resized_image_array = self.__preprocess_image_list(input_image_list, self.model_input_shape[1:3])
+    def inference(self, input_image_list: List[np.ndarray], batch_size: int = 8) -> Tuple[List[Dict], np.array]:
+        resized_image_array, resize_image_shape_list, input_image_shape_list = self.__preprocess_image_list(input_image_list, self.model_input_shape[1:3])
         raw_pred = self.__inference(resized_image_array, batch_size)
-        output = self.__output_parse(raw_pred)
+        output = self.__output_parse(raw_pred, resize_image_shape_list, input_image_shape_list)
         return output, raw_pred
 
     def __inference(self, resize_input_tensor: np.ndarray, batch_size: int) -> np.ndarray:
@@ -36,15 +36,17 @@ class PbModel:
         return output_tensor
 
     def __preprocess_image_list(self, input_image_list: List[np.ndarray],
-                                resize_input_shape: Tuple[int, int]) -> np.ndarray:
-        resized_image_list = []
+                                resize_input_shape: Tuple[int, int]) -> Tuple[np.ndarray, List[Tuple], List[Tuple]]:
+        resized_image_list, resize_image_shape_list, input_image_shape_list = [], [], []
         for input_image in input_image_list:
-            resized_image = self.__preprocess_image(input_image, resize_input_shape)
+            resized_image, resize_image_shape, input_image_shape = self.__preprocess_image(input_image, resize_input_shape)
             resized_image_list.append(resized_image)
-        return np.stack(resized_image_list)
+            resize_image_shape_list.append(resize_image_shape)
+            input_image_shape_list.append(input_image_shape)
+        return np.stack(resized_image_list), resize_image_shape_list, input_image_shape_list
 
     def __preprocess_image(self, input_image: np.ndarray, resize_input_shape: Tuple[int, int]) -> Tuple[
-        np.ndarray, Tuple[float, float]]:
+        np.ndarray, Tuple[int, int], Tuple[int, int]]:
         if len(input_image.shape) != 3:
             raise ValueError('dimension mismatch')
         if not np.issubdtype(input_image.dtype, np.uint8):
@@ -61,11 +63,13 @@ class PbModel:
         resize_pil_image = pil_image.resize(resize_size)
         resize_image = np.array(resize_pil_image)
         output_image[:resize_image.shape[0], :resize_image.shape[1], :] = resize_image
-        return output_image
+        return output_image, resize_image.shape, input_image.shape
 
-    def __output_parse(self, pred: np.ndarray) -> List[Dict]:
+    def __output_parse(self, pred: np.ndarray, resize_image_shape_list: List[Tuple], input_image_shape_list: List[Tuple]) -> List[Dict]:
         output_dict_list = []
         for index in range(pred.shape[0]):
-            output_dict = {'labels': np.argmax(pred[index], -1)}
+            crop_pred = pred[index][:resize_image_shape_list[index][0], :resize_image_shape_list[index][1]]
+            resize_pred = resize(crop_pred, (input_image_shape_list[index]))
+            output_dict = {'labels': np.argmax(resize_pred, -1)}
             output_dict_list.append(output_dict)
         return output_dict_list
